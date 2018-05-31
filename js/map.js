48bd712f74b440bd52b02460e04b2284e07bd3ff
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-05-31 13:52:20 +0100

    [Task] Banner/notification on regular map
    Task url - https://app.asana.com/0/680458161056001/681979960345449/f
    
    - added a global notifications helper function to the map.js file
    - updated the index.html file to call the new function
    - updated the css with the new styles

--- js/map.js
+++ js/map.js
@@ -962,3 +962,104 @@ $(function() {
 	 }
 	}
 });
+
+const uoy_map = (function(){
+
+	const cookieName = 'global-notice-status';
+	let _closeBtnHTML = '<button type="button" class="c-alert__close js-alert-close" aria-label="Close">&times;</button>';
+    let _noticeHTML = '<div class="c-global-notice {0}">{x}{1}{2}</div>';
+    let _noticeTitle = '<h2 class="c-global-notice__title">{0}</h2>';
+    let _closeBtn = '<button type="button" class="c-alert__close js-alert-close" aria-label="Close">&times;</button>'
+	let _defaultOptions = {
+		placeBeforeElement: '.c-map-header',
+		title: '',
+		description: '',
+		noticeModifierClasses: '',
+		closeable: false
+	};
+
+	function stringChecker(inputStr) {
+		return inputStr && inputStr.length > 0;
+	}
+	function stringReplace(template, replaceArr) {
+
+		for(let i = 0; i < replaceArr.length; i++) {
+			placeholder = "{" + i.toString() + "}";
+			template = template.replace(placeholder, replaceArr[i]);
+		}
+
+		return template;
+	}
+
+	// cookies - todo: needs abstracting
+	function setCookie(cookieName, value, expires) {
+		var expiresStr = '';
+		if(expires) {
+            var d = new Date();
+            d.setTime(d.getTime() + (expires * 24 * 60 * 60 * 1000));
+            expiresStr = "expires=" + d.toUTCString() + ";";
+        }
+        document.cookie = cookieName + "=" + value + ";" + expiresStr + "path=/";
+	}
+	function getCookie(cookieName) {
+        var name = cookieName + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+	}
+
+
+	const addGlobalNotice = function(options) {
+		// merge passed in options into defaults
+		$.extend(_defaultOptions, options);
+
+		let $placementEl = $(_defaultOptions.placeBeforeElement),
+			title = stringChecker(_defaultOptions.title) ? stringReplace(_noticeTitle, [_defaultOptions.title]) : '';
+
+		let outputHTML = stringReplace(_noticeHTML, [
+			_defaultOptions.noticeModifierClasses,
+			title,
+			_defaultOptions.description
+		]);
+
+		// todo - do the closeable stuff here
+		if(_defaultOptions.closeable) {
+
+			// add in the close button to the html
+			outputHTML = outputHTML.replace('{x}', _closeBtnHTML);
+
+			// check if the element has already been closed and set by a cookie
+			// if so, hide the element
+            if(getCookie(cookieName) === 'closed' ){
+                //$('.c-global-notice').hide(800);
+	            return; // prevents the global notice being written
+            } else {
+
+                $('div[class=wrapper]').on('click', '.js-alert-close', function (e) {
+                    console.log('alert closed');
+
+                    e.preventDefault();
+                    $('.c-global-notice').hide();
+
+                    // set a cookie to stash the closure status
+                    setCookie(cookieName, 'closed', null);
+                });
+            }
+		}
+
+		$placementEl.before(outputHTML);
+	};
+
+	return {
+		addGlobalNotice : addGlobalNotice
+	}
+})();
