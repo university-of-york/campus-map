22430a68c67ab8ec277637d8f61a39dbf7c942c4
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-06-13 12:11:42 +0100

    Further Codacity refactorings

--- js/pointofinterest.js
+++ js/pointofinterest.js
@@ -3,17 +3,17 @@ const poi_builder = (function(){
     var Popup = null; // null object container which will be populated once constructed.
     const _classNameAnchor = "o-poi-item--anchor",
         _classNameItem = "o-poi-item",
-        _classNameIsHidden = 'is-hidden';
+        _classNameIsHidden = "is-hidden";
 
     function constructPopupClass() {
         Popup = function (position, content) {
             this.position = position;
 
-            let pixelOffset = document.createElement('div');
+            let pixelOffset = document.createElement("div");
             pixelOffset.classList.add(_classNameAnchor);
             pixelOffset.appendChild(content);
 
-            this.anchor = document.createElement('div');
+            this.anchor = document.createElement("div");
             this.anchor.classList.add(_classNameItem);
             // we hide this initially to determine current device and zoom level
             this.anchor.classList.add(_classNameIsHidden);
@@ -44,12 +44,12 @@ const poi_builder = (function(){
             // Hide the popup when it is far out of view.
             var display =
                 Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-                    'block' :
-                    'none';
+                    "block" :
+                    "none";
 
-            if (display === 'block') {
-                this.anchor.style.left = divPosition.x + 'px';
-                this.anchor.style.top = divPosition.y + 'px';
+            if (display === "block") {
+                this.anchor.style.left = divPosition.x + "px";
+                this.anchor.style.top = divPosition.y + "px";
             }
             if (this.anchor.style.display !== display) {
                 this.anchor.style.display = display;
@@ -58,10 +58,10 @@ const poi_builder = (function(){
         /** Stops clicks/drags from bubbling up to the map. */
         Popup.prototype.stopEventPropagation = function() {
             let anchor = this.anchor;
-            anchor.style.cursor = 'auto';
+            anchor.style.cursor = "auto";
 
-            ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
-                'pointerdown']
+            ["click", "dblclick", "contextmenu", "wheel", "mousedown", "touchstart",
+                "pointerdown"]
                 .forEach(function(event) {
                     anchor.addEventListener(event, function(e) {
                         e.stopPropagation();
@@ -75,7 +75,7 @@ const poi_builder = (function(){
     }
 
     function togglePoiItems(visible) {
-        let $poiItems = $('.' + _classNameItem);
+        let $poiItems = $("." + _classNameItem);
 
         if(!visible) {
             $poiItems.addClass(_classNameIsHidden);
@@ -89,8 +89,8 @@ const poi_builder = (function(){
             smallScreen = window.matchMedia("(max-width: 40em)").matches;
 
         // used for older browsers that don't support default parameters
-        zoomLimitSmall = typeof zoomLimitSmall !== 'undefined' ? zoomLimitSmall : 14;
-        zoomLimitLarge = typeof zoomLimitLarge !== 'undefined' ? zoomLimitLarge : 15;
+        zoomLimitSmall = typeof zoomLimitSmall !== "undefined" ? zoomLimitSmall : 14;
+        zoomLimitLarge = typeof zoomLimitLarge !== "undefined" ? zoomLimitLarge : 15;
 
         togglePoiItems(smallScreen && zoomLevel >= zoomLimitSmall || !smallScreen && zoomLevel >= zoomLimitLarge);
     }
