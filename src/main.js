1921b699100b6d849109098bc6f7ee557886049e
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-06-20 11:02:54 +0100

    perf: configured webpack package manager
    BREAKING CHANGE: updated the project to take advantage of webpack package manager.
    
    Also, removed tiles to place them in their own project, which can be optimised

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// webpack variables
+const path = require("path");
 
 // load css files
 require('./css/snazzy-info-window.css');
@@ -7,50 +9,17 @@ require('./css/map.css');
 require("./js/pointofinterest");
 require("./js/uoy-map");
 
-// load assets
-//const imgPath = require.context('./img', true);
-
-//require('./js/map');
-//window.SnazzyInfoWindow = require('./js/snazzy-info-window');
-
 
 if (process.env.NODE_ENV !== "production") {
     console.log("Development mode activated. Start the fans, please!");
 }
 
-// helper script to load JS files dynamically
-function loadJS(filename, onloadCallback) {
-    var fileref = document.createElement('script');
-    fileref.setAttribute("type", "text/javascript");
-    fileref.setAttribute("src", filename);
-    if(onloadCallback && onloadCallback != '') {
-        fileref.setAttribute("onload", onloadCallback);
-    }
-
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref);
-}
-
-//require("./js/map");
-// require("./js/snazzy-info-window");
-const path = require("path");
-
 // kick start the app (note, this loads as part of the Google Maps API callback)
 function initCampusMaps() {
 
-    // load our other scripts because we know the Google and Map objects are available
-    //loadJS('/js/pointofinterest.js');
-    //loadJS('/js/uoy-map.js', "initPostLoad()");
-
-
     // loads up any points of interest and global notices
     uoy_map.init();
-    // loadJS('/js/map.js');
-    // loadJS('/js/snazzy-info-window.js');
-    // require.context("./src/js/map.js");
-    // require.context(path.resolve(__dirname, "src") + "/js/map");
-    // require.context(path.resolve(__dirname, "src") + "/js/snazzy-info-window");
-    //require("./js/map");
+
     require("./js/map");
     window.SnazzyInfoWindow = require('./js/snazzy-info-window');
 }
