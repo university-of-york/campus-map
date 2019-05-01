30d14fdc874de176dda20d3b2c0def488200c90a
Nick Pearson <nick.pearson@york.ac.uk>
2019-05-01 13:07:08 +0100

    Wraps open day layer hiding in try/catch

--- src/js/mapinterface.js
+++ src/js/mapinterface.js
@@ -46,12 +46,15 @@ class MapInterface{
         // });
 
         // Hide irrelevant custom layers for now
-        this.map.on( 'style.load' , () => {
+        // 01/05/2019 - TODO: Remove when no longer relevant
+        try {
+            this.map.on( 'style.load' , () => {
 
-            this.map.setLayoutProperty( "open-day-labels" , "visibility" , "none" );
-            this.map.setLayoutProperty( "open-day-dots" , "visibility" , "none" );
+                this.map.setLayoutProperty( "open-day-labels" , "visibility" , "none" );
+                this.map.setLayoutProperty( "open-day-dots" , "visibility" , "none" );
 
-        })
+            })
+        } catch(e) {}
 
         // We'll use this to keep track of all open popups
         this.openPopups = {};
