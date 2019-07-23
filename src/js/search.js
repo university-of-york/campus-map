e7319c6ca700ab496e3e452319f1c5ca0beea8b6
Dave Hill <dave.hill@york.ac.uk>
2019-07-23 13:42:42 +0100

    Update search results zone badge

--- src/js/search.js
+++ src/js/search.js
@@ -112,7 +112,7 @@ const Search = (function() {
                 $.each(fuseResult, function(i, feature) {
                     // Add title, subtitle, link fields
                     fuseResult[i].item.title = feature.item.properties.title;
-                    fuseResult[i].item.badge = '<span class="campus-zone zone-'+ feature.item.properties.zone+'"></span>';
+                    fuseResult[i].item.badge = '<div class="c-badge"><span class="c-campus-zone zone-'+ feature.item.properties.zone+'"></span><span class="c-campus-zone__label"> Zone '+ feature.item.properties.zone+'</span></div>';
                     fuseResult[i].item.subtitle = feature.item.properties.subtitle;
                     fuseResult[i].item.link = '#' + makeHash(feature.item.properties.locationid);
                     if (i === fuseResult.length - 1) {
